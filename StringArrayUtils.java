2672c535e2dcb02095f5c93dfb6a768e48dd2315
Swathi Bikumalia <swathibikumalia@zipcoders-MBP.hsd1.pa.comcast.net>
2019-02-14 22:11:14 -0500

    implemented

--- StringArrayUtils.java
+++ StringArrayUtils.java
@@ -92,29 +92,12 @@ public class StringArrayUtils {
 
     /**
      * @param array array of String objects
-     * @return true if each letter in the alphabet has been used in the array
-     */ // TODO
-    public static boolean isPangramic(String[] array) {
-        
-        boolean bool = false;
+     * @return true if each letter in the alphabet has been used in the array */
+      // TODO
+      // public static boolean isPangramic(String[] array) {
+        //   return null;
+       //}
 
-        StringBuilder stringBuilder = new StringBuilder();
-
-        for (String x : array){
-            stringBuilder.append(x);
-        }
-
-        String s = "";
-        s = stringBuilder.toString();
-        for(char i = 'A'; i <= 'Z'; i++){
-            if ((s.indexOf(i) < 0) && (s.indexOf((char)i+32)< 0)){
-                bool = false;
-            }else{
-                bool = true;
-            }
-        }
-        return bool;
-    }
 
         
 
@@ -123,16 +106,10 @@ public class StringArrayUtils {
      * @param value value to check array for
      * @return number of occurrences the specified `value` has occurred
      */ // TODO
-    public static int getNumberOfOccurrences(String[] array, String value) {
-        int count = 0;
-        for (int i = 0; i < array.length; i++){
-            if(array[i].equals(value)) {
-                count++;
-            }
-        }
-
-        return count;
-    }
+    //public static int getNumberOfOccurrences(String[] array, String value) {
+      
+      //  return null;
+    //}
 
     
 
@@ -142,16 +119,7 @@ public class StringArrayUtils {
      * @return array with identical contents excluding values of `value`
      */ // TODO
     public static String[] removeValue(String[] array, String valueToRemove) {
-        int count = 0;
-        int occurrences = getNumberOfOccurrences(array, valueToRemove);
-        String[] arr = new String[array.length-occurrences]; 
-        for (String s : array) {
-            if (s != valueToRemove) {
-                arr[count] = s;
-                count++;
-            }
-        }
-        return arr;
+        return null;
     
     }
 
@@ -160,22 +128,7 @@ public class StringArrayUtils {
      * @return array of Strings with consecutive duplicates removes
      */ // TODO
     public static String[] removeConsecutiveDuplicates(String[] array) {
-        String [] arr = new String[array.length];
-        int newArrayIndex = 0;
-
-        arr[newArrayIndex] = array[0];
-
-        newArrayIndex++;
-        for (int i = 1; i < array.length; i++){
-            if(!(arr[newArrayIndex-1]).equals(array[i])){
-                arr[newArrayIndex] = array[i];
-                newArrayIndex++;
-            }
-
-        }
-        String[] newArray = new String[newArrayIndex];
-        System.arraycopy(arr, 0, newArray, 0, newArrayIndex);
-        return newArray;
+       return null;
     }
 
     
@@ -185,39 +138,7 @@ public class StringArrayUtils {
      * @return array of Strings with each consecutive duplicate occurrence concatenated as a single string in an array of Strings
      */ // TODO
     public static String[] packConsecutiveDuplicates(String[] array) {
-         StringBuilder build = new StringBuilder(array[0]);
-
-        String[] newArr = new String[array.length-count(array)]; 
-        int counter = 0; 
-        for(int i = 1; i<array.length; i++){
-            if(array[i-1].equals(array[i])){
-                build.append(array[i]);
-
-            }
-            else{
-                newArr[counter] = build.toString();
-                build.setLength(0);
-                build.append(array[i]);
-                counter++;
-            }
-        }
-        
-        newArr[counter] = build.toString();
-        return newArr;
+         return null;
     }
-
-    public static int count(String[] array){
-        int count = 0;
-        for(int i = 1; i<array.length; i++){
-            if(array[i-1].equals(array[i])) {
-
-                count++;
-            }
-        }
-
-        return count;
-    }
-
-
-    }
+}
 
